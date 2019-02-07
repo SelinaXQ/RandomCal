737b2a73958efb1af1408b4bcbe44e209e5e2749
xiangqian.sx@fosun.com <xiangqian.sx@fosun.com>
2019-02-06 21:45:41 -0500

    first commit

--- app/src/main/java/com/example/administrator/randomcal/RandomAndCal.java
+++ app/src/main/java/com/example/administrator/randomcal/RandomAndCal.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class RandomAndCal {
     public String problem;
+
     public RandomAndCal(String inputProblem){
         setProblem(inputProblem);
     }
